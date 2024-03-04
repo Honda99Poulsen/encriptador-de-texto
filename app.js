aed2d3ef2031dd74a61334037f3d79d61da8807c
EliasZavala <eliaszavala1216@gmail.com>
2024-03-04 11:01:41 -0600

    Actualizacion de app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+function encriptarTexto(){
+    let texto = document.getElementById('texto').value;
+
+    console.log(texto.length);
+
+    let salida = document.getElementById('salidaDatos'). value = texto;
+}
+
