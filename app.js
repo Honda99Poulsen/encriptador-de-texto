50c8b0843f294a450edde22b9a80e4ec08fecfd0
EliasZavala <eliaszavala1216@gmail.com>
2024-03-05 08:50:27 -0600

    Actualizacion del app.js

--- app.js
+++ app.js
@@ -1,58 +1,57 @@
+
+let textoFinal = '';
+
 function encriptarTexto(){
-    let textoFinal = '';
-    let textoFinalDos = '';
+
     let texto = document.getElementById('texto').value;
-
-    function encriptarTexto(){
         
-        for (let i = 0; i < texto.length; i++) {
-            if(texto[i] == 'e'){
-                textoFinal += 'enter';
+    for (let i = 0; i < texto.length; i++) {
+        if(texto[i] == 'e'){
+            textoFinal += 'enter';
+        }else{
+            if(texto[i] == 'i'){
+                textoFinal += 'imes';
             }else{
-                if(texto[i] == 'i'){
-                    textoFinal += 'imes';
-                }else{
-                    if(texto[i] == 'a'){
-                        textoFinal += 'ai';
+                if(texto[i] == 'a'){
+                    textoFinal += 'ai';
+                }else {
+                    if(texto[i] == 'o'){
+                        textoFinal += 'ober';
                     }else {
-                        if(texto[i] == 'o'){
-                            textoFinal += 'ober';
-                        }else {
-                            if(texto[i] == 'u'){
-                                textoFinal += 'ufat';
-                            }else{
-                                textoFinal += texto[i];
-                            }
-                        }
-                    }
-                }
-            }
-            
-        }
-        console.log(texto.length)
-        console.log(textoFinal.length)
-    }
-    encriptarTexto();
-
-    function desencriptarTexto(){
-        for (let i = 0; i < textoFinal.length; i++) {
-            if(textoFinal[i] == 'e'){
-                if(textoFinal[i+1] == 'n'){
-                    if(textoFinal[i+2] == 't'){
-                        if(textoFinal[i+3] == 'e'){
-                            if(textoFinal[i+4] == 'r'){
-                                textoFinalDos += 'e';
-                                console.log(true)
-                            }
+                        if(texto[i] == 'u'){
+                            textoFinal += 'ufat';
+                        }else{
+                            textoFinal += texto[i];
                         }
                     }
                 }
             }
         }
-        console.log(textoFinalDos);
+        
     }
-    desencriptarTexto()
-
+    
     let salida = document.getElementById('salidaDatos'). value = textoFinal;
 }
 
+
+
+
+
+
+function desencriptarTexto(){
+    for (let i = 0; i < textoFinal.length; i++) {
+        if(textoFinal[i] == 'e'){
+            if(textoFinal[i+1] == 'n'){
+                if(textoFinal[i+2] == 't'){
+                    if(textoFinal[i+3] == 'e'){
+                        if(textoFinal[i+4] == 'r'){
+                            textoFinalDos += 'e';
+                            console.log(true)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    console.log(textoFinalDos);
+}
