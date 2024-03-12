b7f1a77fd53e61de7c738dac7e38755e52cb3b31
EliasZavala <eliaszavala1216@gmail.com>
2024-03-12 12:18:16 -0600

    Se ha actualizado app.js

--- app.js
+++ app.js
@@ -1,41 +1,69 @@
 
-let textoFinal = '';
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('texto').addEventListener('input', function(event) {
+        let texto = event.target.value;
+        
+        texto = texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+       
+        texto = texto.toLowerCase();
+        
+        event.target.value = texto;
+    });
+});
 
 function encriptarTexto(){
-
-    let texto = document.getElementById('texto').value;
-        
-    for (let i = 0; i < texto.length; i++) {
-        if(texto[i] == 'e'){
-            textoFinal += 'enter';
-        }else{
-            if(texto[i] == 'i'){
-                textoFinal += 'imes';
-            }else{
-                if(texto[i] == 'a'){
-                    textoFinal += 'ai';
-                }else {
-                    if(texto[i] == 'o'){
-                        textoFinal += 'ober';
-                    }else {
-                        if(texto[i] == 'u'){
-                            textoFinal += 'ufat';
-                        }else{
-                            textoFinal += texto[i];
-                        }
-                    }
-                }
-            }
-        }
-        
-    }
     
-    modificarHTML('salidaDatos', textoFinal);
+    let texto = document.getElementById('texto').value;
+    let newText = texto;
 
+    if(texto == ''){
+        return;;
+    }
+        
+    newText = newText.replace(/e/g, 'enter')
+    newText = newText.replace(/i/g, 'imes')
+    newText = newText.replace(/a/g, 'ai')
+    newText = newText.replace(/o/g, 'ober')
+    newText = newText.replace(/u/g, 'ufaf')
+
+    console.log(texto);
+
+    quitarImagen('mensajeFinal');
+    
+    modificarHTML('salidaDatos', newText);
     modificarHTML('salidaDatosP', '');
 
-    textoFinal = '';
+    modificarCSS('salidaDatos', 'font-size', '24px');
+    modificarCSS('salidaDatos', 'font-weight', '400');
 
+    mostrarBotonPorId('copiar', 'hidden');
+    mostrarBotonPorId('copiar', 'disabled');
+
+}
+
+function quitarImagen(id){
+    let elemento = document.getElementById(id);
+    let imagen = elemento.querySelector('img');
+    if(imagen){
+        imagen.remove();
+    } else {
+        console.log('No se ha encontrado imagen');
+    }
+}
+
+
+function modificarCSS(id, caracteristica, descripcion){
+    let modify = document.getElementById(id);
+    modify.style[caracteristica] = descripcion;
+}
+
+function copiarTexto(){
+    navigator.clipboard.writeText(texto);
+}
+
+function mostrarBotonPorId(id, atributo){
+    let mostrar = document.getElementById(id);
+    mostrar.removeAttribute(atributo);
 }
 
 function modificarHTML(id, text){
@@ -45,19 +73,28 @@ function modificarHTML(id, text){
 
 
 function desencriptarTexto(){
-    for (let i = 0; i < textoFinal.length; i++) {
-        if(textoFinal[i] == 'e'){
-            if(textoFinal[i+1] == 'n'){
-                if(textoFinal[i+2] == 't'){
-                    if(textoFinal[i+3] == 'e'){
-                        if(textoFinal[i+4] == 'r'){
-                            textoFinalDos += 'e';
-                            console.log(true)
-                        }
-                    }
-                }
-            }
-        }
+
+    let texto = document.getElementById('texto').value;
+    console.log(texto);
+    let nuevoTexto = texto;
+    if(texto == ''){
+        return;
     }
-    console.log(textoFinalDos);
+
+    nuevoTexto = nuevoTexto.replace(/enter/g, 'e');
+    nuevoTexto = nuevoTexto.replace(/imes/g, 'i');
+    nuevoTexto = nuevoTexto.replace(/ai/g, 'a');
+    nuevoTexto = nuevoTexto.replace(/ober/g, 'o');
+    nuevoTexto = nuevoTexto.replace(/ufat/g, 'u');
+
+    quitarImagen('mensajeFinal');
+    
+    modificarHTML('salidaDatos', nuevoTexto);
+    modificarHTML('salidaDatosP', '');
+
+    modificarCSS('salidaDatos', 'font-size', '24px');
+    modificarCSS('salidaDatos', 'font-weight', '400');
+
+    mostrarBotonPorId('copiar', 'hidden');
+    mostrarBotonPorId('copiar', 'disabled');
 }
